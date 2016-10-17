f9ad6888a7529bb656b5b6fda12d82c3a8b1a240
fanjingqi <fanjingqi@bearead.cn>
2016-10-17 15:39:36 +0800

    first push

--- TestTravisCI/ViewController.m
+++ TestTravisCI/ViewController.m
@@ -19,6 +19,11 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)configUI
+{
+    NSLog(@"Test Travis CI");
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
