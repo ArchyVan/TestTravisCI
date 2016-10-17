537e61681b47013f968d2fe29e620d1e287f7b5b
fanjingqi <fanjingqi@bearead.cn>
2016-10-17 15:04:15 +0800

    TestTravisCI init

---
+++ TestTravisCI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTravisCI
+//
+//  Created by Objective-C on 2016/10/17.
+//  Copyright © 2016年 Archy Van. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
